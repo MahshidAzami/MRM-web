bcdeaffded7aaf9cfd8a9a55c7b8ea46b0483344
behzadkk <m.azami2008@gmail.com>
2020-09-06 19:33:31 +0100

    build0609

---
+++ precache-manifest.fd55b62012c06fc79b3d1c470f9f1431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd6f1e71a61dcc17a7270790be43b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9702af80efce0cc91d1a",
+    "url": "/static/css/main.b9185ab7.chunk.css"
+  },
+  {
+    "revision": "a6cb22b23b95e1f0789d",
+    "url": "/static/js/2.ad56ed1b.chunk.js"
+  },
+  {
+    "revision": "fe259b219024ba0772538f1e587048bf",
+    "url": "/static/js/2.ad56ed1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9702af80efce0cc91d1a",
+    "url": "/static/js/main.8b015667.chunk.js"
+  },
+  {
+    "revision": "245f59460de7ab016c37",
+    "url": "/static/js/runtime-main.01dd82c3.js"
+  }
+]);
